69e3dc8b4eb93a7a7ff9f84b0704a0d9f8239079
ffiza <fgiza@protonmail.com>
2022-09-05 16:50:46 -0300

    Add functions to plot the inflow and outflow rates and output figures.

--- source/main.py
+++ source/main.py
@@ -90,6 +90,12 @@ class Analysis:
             virial_mass[-1] = virial_masses[galaxy]
             df['VirialMass'] = virial_mass
 
+        # Load inflow and outflow rates only for the reruns.
+        if rerun:
+            data = np.loadtxt(f'{data_path}accretion_tracers_disc_spacing.csv')
+            df['InflowRate'] = data[:, 0]
+            df['OutflowRate'] = data[:, 1]
+
         # Add this data frame to the class dictionary.
         self.dfs[f'Au{galaxy}{rerun_str}'] = df
 
@@ -297,6 +303,86 @@ class Analysis:
         plt.savefig('../images/disc_size_evolution.png')
         plt.close(fig)
 
+    def plot_inflows(self, galaxies: list) -> None:
+        """
+        Creates a plot of the evolution of the inflow rate
+        for a given list of galaxies.
+
+        :param galaxies: A list of galaxy labels to include in the plot.
+        """
+        # Check if rerun is indicated in the labels.
+        for galaxy in galaxies:
+            if '_rerun' not in galaxy:
+                raise Exception("_rerun not found in labels.")
+
+        fig = plt.figure()
+        gs = fig.add_gridspec(ncols=1, nrows=1)
+        ax = gs.subplots(sharex=True, sharey=True)
+
+        ax.tick_params(which='both', direction="in")
+        ax.set_axisbelow(True)
+        ax.grid(True, linestyle='--', lw=.5)
+        ax.set_xlim(0, 14)
+        ax.set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
+        ax.set_ylim(0.1, 300)
+        ax.set_yscale('log')
+        ax.set_yticks([0.1, 1, 10, 100])
+        ax.set_yticklabels([0.1, 1, 10, 100])
+        ax.set_xlabel(r'Tiempo [Gyr]')
+        ax.set_ylabel(r'$\dot{M}_\mathrm{in}$ '
+                      + r'[$\mathrm{M}_\odot \, \mathrm{yr}^{-1}$]')
+
+        for galaxy in galaxies:
+            is_finite = np.isfinite(self.dfs[galaxy]['InflowRate'])
+            ax.plot(self.dfs[galaxy]['Time'][is_finite],
+                    self.dfs[galaxy]['InflowRate'][is_finite],
+                    'o-', lw=0.75, ms=3, mec='White', mew=0.5, label=galaxy)
+
+        self.settings.add_redshift(ax)
+        ax.legend(loc='lower left', framealpha=0, fontsize=6)
+        plt.savefig('../images/inflow_rate.png')
+        plt.close(fig)
+
+    def plot_outflows(self, galaxies: list) -> None:
+        """
+        Creates a plot of the evolution of the outflow rate
+        for a given list of galaxies.
+
+        :param galaxies: A list of galaxy labels to include in the plot.
+        """
+        # Check if rerun is indicated in the labels.
+        for galaxy in galaxies:
+            if '_rerun' not in galaxy:
+                raise Exception("_rerun not found in labels.")
+
+        fig = plt.figure()
+        gs = fig.add_gridspec(ncols=1, nrows=1)
+        ax = gs.subplots(sharex=True, sharey=True)
+
+        ax.tick_params(which='both', direction="in")
+        ax.set_axisbelow(True)
+        ax.grid(True, linestyle='--', lw=.5)
+        ax.set_xlim(0, 14)
+        ax.set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
+        ax.set_ylim(0.1, 300)
+        ax.set_yscale('log')
+        ax.set_yticks([0.1, 1, 10, 100])
+        ax.set_yticklabels([0.1, 1, 10, 100])
+        ax.set_xlabel(r'Tiempo [Gyr]')
+        ax.set_ylabel(r'$\dot{M}_\mathrm{in}$ '
+                      + r'[$\mathrm{M}_\odot \, \mathrm{yr}^{-1}$]')
+
+        for galaxy in galaxies:
+            is_finite = np.isfinite(self.dfs[galaxy]['InflowRate'])
+            ax.plot(self.dfs[galaxy]['Time'][is_finite],
+                    self.dfs[galaxy]['OutflowRate'][is_finite],
+                    'o-', lw=0.75, ms=3, mec='White', mew=0.5, label=galaxy)
+
+        self.settings.add_redshift(ax)
+        ax.legend(loc='lower left', framealpha=0, fontsize=6)
+        plt.savefig('../images/outflow_rate.png')
+        plt.close(fig)
+
 
 if __name__ == '__main__':
     # Initialize class
@@ -311,4 +397,6 @@ if __name__ == '__main__':
     # analysis.plot_disc_to_total_evolution(galaxies=['Au4', 'Au18', 'Au29'])
     # analysis.plot_alignment_dist()
     # analysis.plot_alignment_evolution(galaxies=['Au6', 'Au19', 'Au30'])
-    analysis.plot_disc_size_evolution(galaxies=['Au6', 'Au8', 'Au30'])
+    # analysis.plot_disc_size_evolution(galaxies=['Au6', 'Au8', 'Au30'])
+    analysis.plot_inflows(galaxies=['Au6_rerun', 'Au13_rerun', 'Au28_rerun'])
+    analysis.plot_outflows(galaxies=['Au6_rerun', 'Au13_rerun', 'Au28_rerun'])
